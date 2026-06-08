f53aa737b4755e89c9c317ff543b227e131f8b7d
Kirtanyadav199 <kirtanyadav24@gmail.com>
2026-06-08 14:22:43 +0530

    Learn if types in typescript

---
+++ src/typesInTs.ts
@@ -0,0 +1,11 @@
+let drink = "chai"
+// drink = 0 (not allowed bcoz drink is converted into string type)
+
+let cups = Math.random() > 0.5 ? 10 : 5 ; // cups = number(by default)
+
+let chaiFlavour:string = "masala chai"
+chaiFlavour = "ginger tea"
+
+let chaiOrder : boolean
+
+
